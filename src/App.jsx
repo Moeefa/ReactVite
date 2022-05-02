e68093a3a3671b96ca5c436e8cfaf96b977b29aa
Moeefa <32604322+Moeefa@users.noreply.github.com>
2022-05-01 22:36:42 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ class App extends React.Component {
         <div className="text-center items-center content-center" style={{marginTop: "40px", marginBottom: "60px"}}>
           <p>This project uses <span className="text-white bg-gradient-to-r from-indigo-500 to-purple-700 rounded-full shadow-lg p-1 pr-1.5 pl-1.5">
               React ⚛ + Vite ⚡ and Tailwind 🌬
-            </span> on Repl.it with Node.js v16!</p>
+            </span>!</p>
           <p>Edit and save to test HMR updates.</p>
           <iframe className="block m-0 m-auto rounded-md" frameBorder="0" width="400px" height="400px" src="https://replit.com/@Moeefa/ReactVite?embed=true&lite=true"></iframe>
         </div>
