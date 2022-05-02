bcf6bbd9c3b10949b4d58ebbee200d2d91bda27c
Moeefa <32604322+Moeefa@users.noreply.github.com>
2022-05-01 22:57:25 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react' 
     
 export default defineConfig({
-  define: {
-    PROCESS_VERSION: {v: process.version},
-  },
   plugins: [react()],
   server: {
     hrm: {
